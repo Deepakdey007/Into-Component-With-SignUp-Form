92c7298cc1cb5040d92755ef17d15d94d3ed3640
Deepak Dey <deepakdey593@gmail.com>
2023-05-06 03:04:09 +0530

    some change in js file

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const btn = document.querySelector('[type="submit"]');
 const inputs = document.querySelectorAll("input");
 
 function validateInputs(e) {
-
   e.preventDefault();
 
   let msge = document.querySelector("#message");
@@ -18,42 +17,42 @@ function validateInputs(e) {
       errorIcon.classList.add("visible");
     }
 
-    if(!validateEmail(email.value)) {
-        if(email.value == ""){
-            console.log("empty");
-            msge.textContent = "Email address can not be empty"; 
-        }
-        else{
-            msge.textContent = "Looks like this is not an email"; 
-            email.setAttribute("style", "color:hsl(0, 100%, 74%);border:2px solid hsl(0, 100%, 74%)")
-        }
+    if (!validateEmail(email.value)) {
+      if (email.value == "") {
+        msge.textContent = "Email address can not be empty";
+      } else {
+        msge.textContent = "Looks like this is not an email";
+        email.setAttribute(
+          "style",
+          "color:hsl(0, 100%, 74%);border:2px solid hsl(0, 100%, 74%)"
+        );
+      }
 
-        console.log(input);
-        console.log(email);
+      console.log(errorIcon);
+      console.log(errorText);
       errorText.classList.add("visible");
       errorIcon.classList.add("visible");
-    } 
+    }
 
-    input.addEventListener("input", function () {
-      if (input.value) {
-        // console.log(input);
-
+    input.addEventListener("keyup", function (event) {
+      if (event.key === "Enter" || event.key === "Submit") {
+        errorIcon.classList.remove("visible");
+        errorText.classList.remove("visible");
+      } else if (input.value) {
         input.setAttribute(
           "style",
           "border:2px solid hsl(246, 25%, 77%, 0.3); color:hsl(249, 10%, 26%); font-weight:700"
         );
-        errorIcon.classList.remove("visible");
-        errorText.classList.remove("visible");
       }
     });
   });
 }
 
 function validateEmail(email) {
-    if(email == "") {
-        return "";
-    }
-    var regEx =
+  if (email == "") {
+    return "";
+  }
+  var regEx =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regEx.test(String(email).toLowerCase());
 }
